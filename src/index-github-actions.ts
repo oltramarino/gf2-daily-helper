44d54d41ef6643ca22cd9c43d5ce470c19d6906a
oltramarino <145146123+oltramarino@users.noreply.github.com>
2025-03-13 08:54:02 +0100

    Update index-github-actions.ts

--- src/index-github-actions.ts
+++ src/index-github-actions.ts
@@ -13,7 +13,7 @@ async function handler(event: Event) {
 
         // 任务成功，发送成功通知
         const sendkey = process.env.SERVERCHAN_SENDKEY as string; // 从环境变量获取 sendkey
-        const title = '每日任务完成';
+        const title = '少前2社区每日任务完成';
         const content = '所有每日任务已成功执行。';
         await serverChan(sendkey, title, content);
 
@@ -24,7 +24,7 @@ async function handler(event: Event) {
     } catch (error) {
         // 任务失败，发送失败通知
         const sendkey = process.env.SERVERCHAN_SENDKEY as string; // 从环境变量获取 sendkey
-        const title = '每日任务失败';
+        const title = '少前2社区每日任务失败';
         const content = `任务执行失败，错误信息：${error instanceof Error ? error.message : '未知错误'}`;
         await serverChan(sendkey, title, content);
 
